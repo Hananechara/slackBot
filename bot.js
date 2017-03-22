d96b0ef3f71c77c410251129a5eb6793c691d0f3
CHARA <3674515@ppti-14-503-08.ufr-info-p6.jussieu.fr>
2017-03-22 10:56:54 +0100

    fix2

--- bot.js
+++ bot.js
@@ -14,4 +14,10 @@ module.exports = function(params) {
 	self.onEvent= function(event){
 		console.log(event);
 	}
+	
+	self.onMessage= function(event){
+		if self.bot.type == 'message':
+			if !self.bot.bot_id :
+					self.bot.postMessageToUser(self.bot.user, 'La reponse automatique!!');
+	 }
 }
